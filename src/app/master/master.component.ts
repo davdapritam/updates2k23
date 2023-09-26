809eb39b8632356feb03474ac5188be4b80ad39b
abhiradadiya07 <113245680+abhiradadiya07@users.noreply.github.com>
2023-09-26 15:31:29 +0530

    Faculty Coordinator

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -4,15 +4,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-master',
   templateUrl: './master.component.html',
-  styleUrls: ['./master.component.css']
+  styleUrls: ['./master.component.css'],
 })
 export class MasterComponent implements OnInit {
-
   @ViewChild('myVideo') video!: ElementRef<HTMLVideoElement>;
   @ViewChild('myVideo2') video2!: ElementRef<HTMLVideoElement>;
 
-
-  mainCoordinator = '../../assets/MainCoordinators/keyur.jpeg'
+  mainCoordinator = '../../assets/MainCoordinators/keyur.jpeg';
 
   posters: any[] = [
     { id: 1, img: '../../assets/Events/1.jpg' },
@@ -28,36 +26,94 @@ export class MasterComponent implements OnInit {
     { id: 11, img: '../../assets/Events/11.png' },
     { id: 12, img: '../../assets/Events/12.jpg' },
     { id: 13, img: '../../assets/Events/13.jpg' },
-  ]
+  ];
 
   posterMakingTeam: any[] = [
-    { id: 1, img: '../../assets/Postermaking/nehul.png', name: "Nehul Vasava" },
-    { id: 2, img: '../../assets/Postermaking/Drashti_Gor.png', name: "Drashti Gor" },
-    { id: 3, img: '../../assets/Postermaking/Shruti_Savani.png', name: "Shruti Savani" },
-    { id: 4, img: '../../assets/Postermaking/Abdul_Kadir.png', name: "Abdul Kadir" },
-    { id: 5, img: '../../assets/Postermaking/Kirtan_Kothwala.png', name: "Kirtan Kothwala" },
-    { id: 6, img: '../../assets/Postermaking/Jiya_Patel.png', name: "Jiya Patel" },
-    { id: 7, img: '../../assets/Postermaking/Prerna_Gattani.png', name: "Prerna_Gattani" },
-  ]
+    { id: 1, img: '../../assets/Postermaking/nehul.png', name: 'Nehul Vasava' },
+    {
+      id: 2,
+      img: '../../assets/Postermaking/Drashti_Gor.png',
+      name: 'Drashti Gor',
+    },
+    {
+      id: 3,
+      img: '../../assets/Postermaking/Shruti_Savani.png',
+      name: 'Shruti Savani',
+    },
+    {
+      id: 4,
+      img: '../../assets/Postermaking/Abdul_Kadir.png',
+      name: 'Abdul Kadir',
+    },
+    {
+      id: 5,
+      img: '../../assets/Postermaking/Kirtan_Kothwala.png',
+      name: 'Kirtan Kothwala',
+    },
+    {
+      id: 6,
+      img: '../../assets/Postermaking/Jiya_Patel.png',
+      name: 'Jiya Patel',
+    },
+    {
+      id: 7,
+      img: '../../assets/Postermaking/Prerna_Gattani.png',
+      name: 'Prerna_Gattani',
+    },
+  ];
 
   mainCoordinators = [
-    { id: 1, img: '../../assets/MainCoordinators/akshay.jpeg', name: 'Akshay Golakiya' },
-    { id: 2, img: "../../assets/MainCoordinators/smit.jpg", name: 'Smit Pansuriya' },
-    { id: 3, img: "../../assets/MainCoordinators/ayush.jpeg", name: 'Ayush Kayasth' },
-    { id: 4, img: "../../assets/MainCoordinators/ansh.jpg", name: 'Ansh Mangukiya' },
-    { id: 5, img: "../../assets/MainCoordinators/priya.jpeg", name: 'Priya Sisodiya' },
-    { id: 6, img: "../../assets/MainCoordinators/krish.jpg", name: 'Krish Padmani' },
+    {
+      id: 1,
+      img: '../../assets/MainCoordinators/akshay.jpeg',
+      name: 'Akshay Golakiya',
+    },
+    {
+      id: 2,
+      img: '../../assets/MainCoordinators/smit.jpg',
+      name: 'Smit Pansuriya',
+    },
+    {
+      id: 3,
+      img: '../../assets/MainCoordinators/ayush.jpeg',
+      name: 'Ayush Kayasth',
+    },
+    {
+      id: 4,
+      img: '../../assets/MainCoordinators/ansh.jpg',
+      name: 'Ansh Mangukiya',
+    },
+    {
+      id: 5,
+      img: '../../assets/MainCoordinators/krish.jpg',
+      name: 'Krish Padmani',
+    },
+    {
+      id: 6,
+      img: '../../assets/MainCoordinators/priya.jpeg',
+      name: 'Priya Sisodiya',
+    },
   ];
 
   slides = [
-    { id: 1, img: '../../assets/FacultyCoordinators/vandanaJoshi.jpeg', name: 'Prof. Vandana joshi' },
-    { id: 2, img: '../../assets/FacultyCoordinators/nitiyaKomalan.jpeg', name: 'Ms. Nitya komalan' },
-    { id: 3, img: "../../assets/FacultyCoordinators/yuktiDesai.jpeg", name: 'Ms. Yukti Desai' },
+    {
+      id: 1,
+      img: '../../assets/FacultyCoordinators/vandanaJoshi.jpeg',
+      name: 'Prof. Vandana joshi',
+    },
+    {
+      id: 2,
+      img: '../../assets/FacultyCoordinators/nitiyaKomalan.jpeg',
+      name: 'Ms. Nitya komalan',
+    },
+    {
+      id: 3,
+      img: '../../assets/FacultyCoordinators/yuktiDesai.jpeg',
+      name: 'Ms. Yukti Desai',
+    },
   ];
 
-
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     // this.playVideo();
@@ -69,7 +125,6 @@ export class MasterComponent implements OnInit {
     var video1 = document.getElementById('video1');
     var video2 = document.getElementById('video2');
     if (hiddencomponent && video1 && video2) {
-      console.log("1")
       hiddencomponent.click();
       // hiddencomponent.style.display = 'none';
     }
@@ -82,8 +137,6 @@ export class MasterComponent implements OnInit {
   }
 
   playVideo() {
-
-    console.log("2")
     if (this.video) {
       this.video.nativeElement.play();
     }
@@ -93,11 +146,10 @@ export class MasterComponent implements OnInit {
   }
 
   goToLink(url: string) {
-    window.open(url, "_blank")
+    window.open(url, '_blank');
   }
 
   navigateTo(id: number) {
-    this.router.navigate(['updates2k23/rules', id])
+    this.router.navigate(['updates2k23/rules', id]);
   }
-
 }
